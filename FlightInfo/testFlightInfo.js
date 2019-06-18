96e0709436e81a2e3fdb06c55398c3908b156804
Hun Lee <dan.hun.lee@gmail.com>
2019-06-18 12:42:37 -0700

    gc finished first branch

--- FlightInfo/testFlightInfo.js
+++ FlightInfo/testFlightInfo.js
@@ -22,10 +22,14 @@ $("#find-flight").on("click", function (event) {
                 $(".Depart").text("Depart: " + flightFrom + " " + flightDateFrom);
                 $(".Return").text("Return: "+ flightTo + " " + flightDateTo);
                 // $(".cheapestAirline").text("Airline: " + response.tripset[0].cheapestProviderName);
-                $(".cheapestPrice").text("Cheapest Price: $" + response.cheapestPrice);
+                // $(".cheapestPrice").text("Cheapest Price: $" + response.cheapestPrice);
                 $(".logos").text(JSON.stringify(response.airlineLogos.AA));
                 $(".trip1").text("Trip 1: "+ response.tripset[0].cheapestProviderName + " " + response.tripset[0].displayLow);
                 $(".trip2").text("Trip 2: "+ response.tripset[1].cheapestProviderName + " " + response.tripset[1].displayLow);
+                $(".trip3").text("Trip 3: "+ response.tripset[2].cheapestProviderName + " " + response.tripset[2].displayLow);
+                $(".trip4").text("Trip 4: "+ response.tripset[3].cheapestProviderName + " " + response.tripset[3].displayLow);
+                $(".trip5").text("Trip 5: "+ response.tripset[4].cheapestProviderName + " " + response.tripset[4].displayLow);
+                $(".trip6").text("Trip 6: "+ response.tripset[5].cheapestProviderName + " " + response.tripset[5].displayLow);
 
                 console.log(response.tripset[0]);
             });
